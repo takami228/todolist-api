41522c317e948a3350bf2ad85277807a835e667a
takami228 <m.takami228@gmail.com>
2017-06-23 17:18:40 +0900

    Add TestCode

--- src/test/java/com/example/todolistapi/TodolistApiApplicationTests.java
+++ src/test/java/com/example/todolistapi/TodolistApiApplicationTests.java
@@ -1,16 +1,29 @@
 package com.example.todolistapi;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class TodolistApiApplicationTests {
+
+	TestRestTemplate restTemplate = new TestRestTemplate();
+
+	@LocalServerPort
+	int port;
 
 	@Test
 	public void contextLoads() {
+		assertThat(restTemplate.getForObject("http://localhost:" + port, String.class), is("Document Root"));
 	}
 
 }
